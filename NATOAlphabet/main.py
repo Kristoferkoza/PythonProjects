db42c0bb1b27a06433fef87327bce14410861187
Krzysztof Kozakiewicz <101817983+Kristoferkoza@users.noreply.github.com>
2025-01-27 11:22:10 +0100

    Add files via upload

---
+++ NATOAlphabet/main.py
@@ -0,0 +1,18 @@
+import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+data_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+
+
+def generate():
+    word = input("Enter a word: ").upper()
+    try:
+        result = [data_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate()
+    else:
+        print(result)
+
+
+generate()
